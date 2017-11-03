b291124149f68488a92521898f717ebe648b3494
GOBIG-CASH <33349297+GOBIG-CASH@users.noreply.github.com>
2017-11-03 12:41:51 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php //Edit these only ///////////////////////////////$coin1 = "Coin1";$coin8 = "Coin2";$coin7 = "Coin3";$coin4 = "Coin4";$coin3 = "Coin5";$coin6 = "Coin6";$coin5 = "Coin7";$coin2 = "Coin8";?>
+ <div  id="counter">The coin will be shown here </div> <script src="https://cdn.jsdelivr.net/snap.svg/0.1.0/snap.svg-min.js"></script><svg id="svgout" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="6in" height="6in" version="1.1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"viewBox="0 0 1667 1667" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Wheelmax">  <circle id="Outercycle" fill="none" cx="833" cy="833" r="729"/>  <circle fill="yellow" cx="833" cy="104" r="23"/>  <g id="_894508911376">   <path fill="yellow" d="M628 110c13,0 24,11 24,24 0,12 -11,23 -24,23 -13,0 -23,-11 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M439 196c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M283 332c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path fill="yellow" d="M170 507c13,0 24,10 24,23 0,13 -11,23 -24,23 -12,0 -23,-10 -23,-23 0,-13 11,-23 23,-23z"/>   <path fill="yellow" d="M112 706c13,0 24,10 24,23 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-23 23,-23z"/>   <path fill="yellow" d="M112 913c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path fill="yellow" d="M170 1112c13,0 24,10 24,23 0,13 -11,24 -24,24 -12,0 -23,-11 -23,-24 0,-13 11,-23 23,-23z"/>   <path fill="yellow" d="M283 1286c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path fill="yellow" d="M439 1422c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M628 1509c13,0 24,10 24,23 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-23 23,-23z"/>   <path fill="yellow" d="M833 1538c13,0 24,11 24,24 0,12 -11,23 -24,23 -13,0 -23,-11 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M1039 1509c13,0 23,10 23,23 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-23 24,-23z"/>   <path fill="yellow" d="M1227 1422c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M1384 1286c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M1496 1112c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path fill="yellow" d="M1555 913c13,0 23,10 23,23 0,13 -10,24 -23,24 -13,0 -24,-11 -24,-24 0,-13 11,-23 24,-23z"/>   <path fill="yellow" d="M1555 706c13,0 23,10 23,23 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-23 24,-23z"/>   <path fill="yellow" d="M1496 507c13,0 24,10 24,23 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-23 23,-23z"/>   <path fill="yellow" d="M1384 332c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" d="M1227 196c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>  </g>  <circle id="on" fill="yellow" stroke="black" stroke-width="3" cx="1039" cy="133" r="23"/>  <circle id="Outercycle_0" fill="none" transform="matrix(0.941026 -0.119928 0.119928 0.941026 835.128 834.278)" r="768"/>  <circle fill="black" stroke="black" stroke-width="3" cx="743" cy="111" r="23"/>  <g id="_894526597584">   <path id="off" fill="black" stroke="black" stroke-width="3" d="M543 143c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M367 253c13,0 23,10 23,23 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M229 407c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M139 594c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M107 799c12,0 23,11 23,24 0,13 -11,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M133 1005c13,0 23,10 23,23 0,13 -10,24 -23,24 -13,0 -24,-11 -24,-24 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M216 1195c13,0 23,10 23,23 0,13 -10,24 -23,24 -13,0 -24,-11 -24,-24 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M349 1354c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M522 1469c13,0 23,10 23,23 0,13 -10,24 -23,24 -13,0 -24,-11 -24,-24 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M720 1530c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M927 1534c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1127 1478c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1303 1369c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1442 1214c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1531 1027c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1564 823c13,0 23,10 23,23 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1538 617c12,0 23,10 23,23 0,13 -11,24 -23,24 -13,0 -24,-11 -24,-24 0,-13 11,-23 24,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1454 427c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1321 268c13,0 23,10 23,23 0,13 -10,24 -23,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>   <path id="off" fill="black" stroke="black" stroke-width="3" d="M1148 153c13,0 24,10 24,23 0,13 -11,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-23 23,-23z"/>  </g>  <circle id="off" fill="black" stroke="black" stroke-width="3" cx="950" cy="115" r="23"/>  <circle id="Outercycle_1" fill="none" cx="833" cy="833" r="729"/>  <circle fill="yellow" stroke="black" stroke-width="3" cx="833" cy="104" r="23"/>  <g id="_894526620112">   <path fill="yellow" stroke="black" stroke-width="3" d="M628 110c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M439 196c13,0 24,11 24,24 0,13 -11,23 -24,23 -13,0 -23,-10 -23,-23 0,-13 10,-24 23,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M283 332c13,0 23,11 23,24 0,13 -10,23 -23,23 -13,0 -24,-10 -24,-23 0,-13 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M170 506c14,0 24,11 24,24 0,13 -10,24 -24,24 -13,0 -23,-11 -23,-24 0,-13 10,-24 23,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M112 705c13,0 24,11 24,24 0,13 -11,24 -24,24 -13,0 -24,-11 -24,-24 0,-13 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M112 913c13,0 24,10 24,23 0,14 -11,24 -24,24 -13,0 -24,-10 -24,-24 0,-13 11,-23 24,-23z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M170 1112c14,0 24,10 24,23 0,14 -10,24 -24,24 -13,0 -23,-10 -23,-24 0,-13 10,-23 23,-23z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M283 1286c13,0 24,11 24,24 0,13 -11,24 -24,24 -13,0 -24,-11 -24,-24 0,-13 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M439 1422c14,0 24,10 24,24 0,13 -10,24 -24,24 -13,0 -24,-11 -24,-24 0,-14 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M628 1508c13,0 24,11 24,24 0,13 -11,24 -24,24 -13,0 -24,-11 -24,-24 0,-13 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M833 1537c14,0 25,11 25,25 0,13 -11,24 -25,24 -13,0 -24,-11 -24,-24 0,-14 11,-25 24,-25z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1039 1508c13,0 24,11 24,24 0,13 -11,24 -24,24 -14,0 -24,-11 -24,-24 0,-13 10,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1227 1421c14,0 25,11 25,25 0,13 -11,24 -25,24 -13,0 -24,-11 -24,-24 0,-14 11,-25 24,-25z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1384 1286c13,0 24,10 24,24 0,13 -11,24 -24,24 -13,0 -24,-11 -24,-24 0,-14 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1496 1111c14,0 25,11 25,24 0,14 -11,25 -25,25 -13,0 -24,-11 -24,-25 0,-13 11,-24 24,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1555 912c13,0 24,11 24,24 0,14 -11,25 -24,25 -14,0 -25,-11 -25,-25 0,-13 11,-24 25,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1555 705c13,0 24,10 24,24 0,14 -11,25 -24,25 -14,0 -25,-11 -25,-25 0,-14 11,-24 25,-24z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1496 505c14,0 25,11 25,25 0,14 -11,25 -25,25 -13,0 -24,-11 -24,-25 0,-14 11,-25 24,-25z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1384 331c14,0 25,11 25,25 0,13 -11,24 -25,24 -14,0 -25,-11 -25,-24 0,-14 11,-25 25,-25z"/>   <path fill="yellow" stroke="black" stroke-width="3" d="M1227 195c14,0 25,11 25,25 0,13 -11,24 -25,24 -13,0 -24,-11 -24,-24 0,-14 11,-25 24,-25z"/>  </g>  <circle id="on_2" fill="yellow" stroke="black" stroke-width="3" cx="1039" cy="133" r="25"/>  <circle fill="none" stroke="black" stroke-width="6.94488" cx="833" cy="833" r="766"/>  <g id="wheel1">   <g id="wheel">    <path fill="#2d1d1d" stroke="black" stroke-width="6.94488" d="M833 833l645 -267 0 0c-69,-165 -200,-304 -377,-377l0 0 -267 642 -1 2z"/>    <path fill="#840000" stroke="black" stroke-width="6.94488" d="M833 833l643 268 1 0c68,-165 74,-355 1,-533l0 0 -643 264 -2 1z"/>    <path fill="#2d1d1d" stroke="black" stroke-width="6.94488" d="M833 833l267 644 0 0c165,-68 304,-199 377,-377l0 0 -642 -266 -2 -1z"/>    <path fill="#840000" stroke="black" stroke-width="6.94488" d="M833 833l-266 645 1 0c165,68 356,72 533,-1l0 0 -267 -642 -1 -2z"/>    <path fill="#2d1d1d" stroke="black" stroke-width="6.94488" d="M833 833l-643 269 0 1c69,164 201,302 379,375l0 0 263 -644 1 -1z"/>    <path fill="#840000" stroke="black" stroke-width="6.94488" d="M833 832l-646 -262 0 1c-67,165 -71,356 4,533l0 0 640 -271 2 -1z"/>    <path fill="#2d1d1d" stroke="black" stroke-width="6.94488" d="M834 833l-272 -642 0 0c-164,69 -302,201 -375,380l0 0 645 261 2 1z"/>    <path fill="#840000" stroke="black" stroke-width="6.94488" d="M834 834l266 -645 -1 0c-165,-68 -355,-73 -533,1l0 0 267 642 1 2z"/>   </g> <text font-weight='bold' fill="#840000" y="550" x="1050" font-size="100" transform="rotate(-36 1150,550)" ><?php echo $coin1;?> </text>  <text font-weight='bold'  y="888" x="1119" font-size="100"  ><?php echo $coin2;?> </text>   <text fill="#840000" y="1196" x="400" font-size="100"  transform="rotate(150 500,1146)" ><?php echo $coin3;?> </text>    <text font-weight='bold'  y="891" x="300" font-size="100" transform="rotate(180 410,846)"  ><?php echo $coin4;?> </text>     <text font-weight='bold' fill="#840000"  y="1194" x="1056" font-size="100"  transform="rotate(36 1200,1050)" ><?php echo $coin5;?> </text>      <text font-weight='bold'  y="1300" x="684" font-size="100"  transform="rotate(90 804,1300)" ><?php echo $coin6;?> </text><text font-weight='bold' fill="#840000" y="728" x="307" font-size="100"  transform="rotate(210 427,588)" ><?php echo $coin7;?> </text> <text font-weight='bold' y="521" x="700" font-size="100"  transform="rotate(-90 750,421)" ><?php echo $coin8;?> </text></g> <g >    <polygon id="r44" fill='green' transform="rotate(-90 1250,-200)" points="250,60 100,400 400,400" class="triangle" />     </g> </g>	 <text fill='blue' y="1650" x="550" font-size="40"  ><a href='https://Kvancrypto.com'>Made By Edza48  Kvancrypto.com</a> </text></svg>
+<a id='start' class='btn btn-default' role='button' >SPIN </a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/wheel.css">
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   <script>
+
+var s = Snap("#svgout"); 
+var t = s.select("#wheel1");
+var r44 = s.select("#r44");
+
+
+
+
+
+
+
+
+
+
+
+
+
+var spinrounds =360*20;
+ var timerun = 12000;
+var check= 1;
+
+$( "#start" ).click(function() {
+	
+	document.getElementById("start").outerHTML = "<a class='btn btn-default' role='button' href='index.php?submit=reset'>RESET </a>";
+
+    ran = Math.floor((Math.random() * 360) + spinrounds);
+	coinnr = ran-spinrounds;
+coinnr2 = coinnr/360;
+coinnr3 = coinnr2 *8;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (coinnr3>0.43&coinnr3<1.43){
+	coinname="<?php echo $coin1;?>";
+};
+if (coinnr3>1.43&coinnr3<2.43){
+	coinname="<?php echo $coin8;?>";
+};
+if (coinnr3>2.43&coinnr3<3.43){
+	coinname="<?php echo $coin7;?>";
+};
+if (coinnr3>3.43&coinnr3<4.43){
+	coinname="<?php echo $coin4;?>";
+};
+if (coinnr3>4.43&coinnr3<5.43){
+	coinname="<?php echo $coin3;?>";
+};
+if (coinnr3>5.43&coinnr3<6.43){
+	coinname="<?php echo $coin6;?>";
+};
+if (coinnr3>6.43&coinnr3<7.43){
+	coinname="<?php echo $coin5;?>";
+};
+if (coinnr3>7.43){
+	coinname="<?php echo $coin2;?>";
+};
+if (coinnr3<0.43){
+	coinname="<?php echo $coin2;?>";
+};
+
+	t.animate({ transform: 'r'+ran+',833,833' }, timerun, mina.easeinout );
+	r44.animate({ fill: 'red' }, timerun, mina.easeinout );
+	//	console.log(ran);
+		//console.log(coinnr);
+		//console.log(coinnr2);
+	//	console.log(coinnr3);
+		//console.log(coinname);
+var x = setInterval(function() {
+    document.getElementById("counter").innerHTML = "The winner is = "+coinname;
+}, timerun+1000);
+
+});
+
+</script>
+
+
